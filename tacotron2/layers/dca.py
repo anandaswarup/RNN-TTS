ddc5743759c03f1ee077174ba3ca854649bbb6af
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2022-03-03 18:23:32 +0530

    Dynamic Convolution Attention

--- tacotron2/layers/dca.py
+++ tacotron2/layers/dca.py
@@ -16,9 +16,9 @@ class DynamicConvolutionAttention(nn.Module):
         query_dim,
         attn_dim,
         n_static_filters,
-        static_kernel_size,
+        static_filter_size,
         n_dynamic_filters,
-        dynamic_kernel_size,
+        dynamic_filter_size,
         prior_filter_len,
         alpha,
         beta,
@@ -29,15 +29,15 @@ class DynamicConvolutionAttention(nn.Module):
 
         self.prior_filter_len = prior_filter_len
         self.n_dynamic_filters = n_dynamic_filters
-        self.dynamic_kernel_size = dynamic_kernel_size
+        self.dynamic_filter_size = dynamic_filter_size
 
         prior_filter = betabinom.pmf(np.arange(prior_filter_len), prior_filter_len - 1, alpha, beta)
         self.register_buffer("prior_filter", torch.FloatTensor(prior_filter).flip(0))
 
         self.W = nn.Linear(query_dim, attn_dim)
-        self.V = nn.Linear(attn_dim, n_dynamic_filters * dynamic_kernel_size, bias=False)
+        self.V = nn.Linear(attn_dim, n_dynamic_filters * dynamic_filter_size, bias=False)
 
-        self.F = nn.Conv1d(1, n_static_filters, static_kernel_size, padding=(static_kernel_size - 1) // 2, bias=False)
+        self.F = nn.Conv1d(1, n_static_filters, static_filter_size, padding=(static_filter_size - 1) // 2, bias=False)
 
         self.U = nn.Linear(n_static_filters, attn_dim, bias=False)
         self.T = nn.Linear(n_dynamic_filters, attn_dim)
@@ -52,8 +52,8 @@ class DynamicConvolutionAttention(nn.Module):
         G = self.V(torch.tanh(self.W(query)))
         g = F.conv1d(
             prev_alignment.unsqueeze(0),
-            G.view(-1, 1, self.dynamic_kernel_size),
-            padding=(self.dynamic_kernel_size - 1) // 2,
+            G.view(-1, 1, self.dynamic_filter_size),
+            padding=(self.dynamic_filter_size - 1) // 2,
             groups=query.size(0),
         )
         g = g.view(query.size(0), self.n_dynamic_filters, -1).transpose(1, 2).contiguous()
