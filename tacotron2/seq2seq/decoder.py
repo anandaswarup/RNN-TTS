423f861128d6f48897473660059454c0c639abeb
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2022-03-07 11:37:12 +0530

    Tacotron2 seq2seq Decoder

--- tacotron2/seq2seq/decoder.py
+++ tacotron2/seq2seq/decoder.py
@@ -48,7 +48,7 @@ class Decoder(nn.Module):
         self.attn_lstm = nn.LSTMCell(prenet[-1] + memory_dim, attn_lstm_size)
 
         # Attention Mechanism
-        self.attention = DynamicConvolutionAttention(
+        self.dca = DynamicConvolutionAttention(
             attn_lstm_size,
             attn_dim,
             n_static_filters,
@@ -66,12 +66,11 @@ class Decoder(nn.Module):
         # Output Layer
         self.acoustic_layer = Linear(decoder_lstm_size + memory_dim, n_mels * reduction_factor, bias=True)
 
-    def _init_states(self, memory):
+    def _init_decoder_states(self, memory):
         """Initialize the decoder states
         """
-        B, T_enc, _ = memory.size()
+        B, _, _ = memory.size()
 
-        self.alignment = F.one_hot(torch.zeros([B], dtype=torch.long, device=memory.device), T_enc).float()
         self.attention_context = torch.zeros([B, self.memory_dim], device=memory.device)
 
         self.attn_lstm_hx = [
@@ -109,11 +108,8 @@ class Decoder(nn.Module):
         )
         self.attn_lstm_hx[0] = F.dropout(self.attn_lstm_hx[0], p=self.lstm_dropout, training=self.training)
 
-        # Attention mechanism computes the alignment for the current timestep using the query and previous timestep
-        # alignment
-        alignment = self.attention(self.attn_lstm_hx[0], self.alignment)
-        self.attention_context = torch.matmul(alignment.unsqueeze(1), memory).squeeze(1)
-        self.alignment = alignment
+        # Attention mechanism computes the context for the current timestep using the query and memory
+        self.attention_context = self.dca(self.attn_lstm_hx[0], memory)
 
         # Decoder LSTM
         decoder_lstm_input = torch.cat((self.attn_lstm_hx[0], self.attention_context), dim=-1)
@@ -126,7 +122,7 @@ class Decoder(nn.Module):
         decoder_lstm_output = torch.cat((self.decoder_lstm_hx[0], self.attention_context), dim=-1)
         output_mel_frame = self.acoustic_layer(decoder_lstm_output).view(B, N, self.reduction_factor)
 
-        return output_mel_frame, alignment
+        return output_mel_frame, self.dca.attention_weights
 
     def forward(self, mels, memory):
         """Forward pass (Training in teacher forcing mode: previous timestep ground-truth is used as input while 
@@ -135,16 +131,18 @@ class Decoder(nn.Module):
         T_mels = mels.size(-1)
         mels = mels.unbind(-1)
 
-        self._init_states(memory)
+        self.dca._init_attention(memory)
+        self._init_decoder_states(memory)
+
         go_frame = self._get_go_frame(memory)
 
         output_mels, alignments = [], []
         for t in range(0, T_mels, self.reduction_factor):
             mel_frame = mels[t - 1] if t > 0 else go_frame
-            output_mel_frame, alignment = self.step(mel_frame, memory)
+            output_mel_frame, attention_weights = self.step(mel_frame, memory)
 
             output_mels.append(output_mel_frame)
-            alignments.append(alignment)
+            alignments.append(attention_weights)
 
         output_mels = torch.cat(output_mels, dim=-1)
         alignments = torch.stack(alignments, dim=2)
@@ -154,19 +152,21 @@ class Decoder(nn.Module):
     def inference(self, memory, max_length=10000, stop_threshold=-0.2):
         """Inference mode (Generates mels using greedy decoding)
         """
-        self._init_states(memory)
+        self.dca._init_attention(memory)
+        self._init_decoder_states(memory)
+
         go_frame = self._get_go_frame(memory)
 
         output_mels, alignments = [], []
         for t in range(0, max_length, self.reduction_factor):
             mel_frame = output_mels[-1][:, :, -1] if t > 0 else go_frame
-            output_mel_frame, alignment = self.step(mel_frame, memory)
+            output_mel_frame, attention_weights = self.step(mel_frame, memory)
 
             if torch.all(mel_frame[:, :, -1] > stop_threshold):
                 break
 
             output_mels.append(output_mel_frame)
-            alignments.append(alignment)
+            alignments.append(attention_weights)
 
         output_mels = torch.cat(output_mels, dim=-1)
         alignments = torch.stack(alignments, dim=2)
