5bfd4570d557d46b237112775df7537668a55157
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2022-03-16 12:35:11 +0530

    Transformations that run over the input English language text at training and eval time

--- text/en/cleaners.py
+++ text/en/cleaners.py
@@ -8,6 +8,10 @@ from unidecode import unidecode
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r"\s+")
 
+# Regular expression patterns for puncutations
+parentheses_pattern = re.compile(r"(?<=[.,!?] )[\(\[]|[\)\]](?=[.,!?])|^[\(\[]|[\)\]]$")
+dash_pattern = re.compile(r"(?<=[.,!?] )-- ")
+
 # List of (regular expression, replacement) pairs for abbreviations:
 _abbreviations = [
     (re.compile("\\b%s\\." % x[0], re.IGNORECASE), x[1])
@@ -52,12 +56,36 @@ def convert_to_ascii(text):
     return unidecode(text)
 
 
+def replace_symbols(text):
+    # replace semi-colons and colons with commas
+    text = text.replace(";", ",")
+    text = text.replace(":", ",")
+
+    # replace dashes with commas
+    text = dash_pattern.sub("", text)
+    text = text.replace(" --", ",")
+    text = text.replace(" - ", ", ")
+
+    # split hyphenated words
+    text = text.replace("-", " ")
+
+    # replace parentheses with commas
+    text = parentheses_pattern.sub("", text)
+    text = text.replace(")", ",")
+    text = text.replace(" (", ", ")
+    text = text.replace("]", ",")
+    text = text.replace(" [", ", ")
+
+    return text
+
+
 def clean_text(text):
     """Pipeline for English text, including number and abbreviation expansion
     """
     text = convert_to_ascii(text)
     text = expand_numbers(text)
     text = expand_abbreviations(text)
+    text = replace_symbols(text)
     text = collapse_whitespace(text)
 
     return text
