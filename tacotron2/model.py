42b355a18e4f00a90a65cc702a6a61af1a9ab29e
Anandaswarup Vadapalli <anandaswarup.vadapalli@gmail.com>
2022-06-12 17:24:51 +0530

    Tacotron2 model

--- tacotron2/model.py
+++ tacotron2/model.py
@@ -8,6 +8,7 @@ from text.en.processor import symbol_to_id
 
 from tacotron2.seq2seq.decoder import Decoder
 from tacotron2.seq2seq.encoder import Encoder
+from tacotron2.seq2seq.postnet import PostNet
 
 
 class Tacotron2(nn.Module):
@@ -57,21 +58,48 @@ class Tacotron2(nn.Module):
             cfg.tts_model["decoder"]["reduction_factor"],
         )
 
+        # Post Processing Network
+        self.postnet = PostNet(
+            cfg.audio["n_mels"],
+            cfg.tts_model["postnet"]["n_conv_layers"],
+            cfg.tts_model["postnet"]["n_conv_filters"],
+            cfg.tts_model["postnet"]["conv_filter_size"],
+            cfg.tts_model["postnet"]["conv_dropout"],
+        )
+
     def forward(self, texts, mels):
         """Forward pass
         """
+        # Embedding layer
         embedded_texts = self.embedding_layer(texts)
+
+        # Text Encoder
         memory = self.encoder(embedded_texts)
+
+        # Autoregressive Decoder
         output_mels, alignments = self.decoder(mels, memory)
 
-        return output_mels, alignments
+        # Post Processing Network
+        postnet_output_mels = self.postnet(output_mels)
+        postnet_output_mels = output_mels + postnet_output_mels
+
+        return postnet_output_mels, output_mels, alignments
 
     def generate(self, texts):
         """Generate mels from text
         """
+        # Embedding layer
         embedded_texts = self.embedding_layer(texts)
+
+        # Text Encoder
         memory = self.encoder(embedded_texts)
+
+        # Autoregressive Decoder
         output_mels, alignments = self.decoder.inference(memory)
 
-        return output_mels, alignments
+        # Post Processing Network
+        postnet_output_mels = self.postnet(output_mels)
+        postnet_output_mels = output_mels + postnet_output_mels
+
+        return postnet_output_mels, output_mels, alignments
 
